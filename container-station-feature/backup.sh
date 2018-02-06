1ed537a4811563f5b8a9e63e0a42da3572d06753
Alex Yang <alexyang@qnap.com>
2018-02-06 20:30:01 +0800

    feat: handle SIGINT

--- container-station-feature/backup.sh
+++ container-station-feature/backup.sh
@@ -1,20 +1,70 @@
 #!/bin/sh -e
 
+function stop()
+{
+    while true; do
+        read -rep $'\nStop backup script?(y/n)' yn
+        case $yn in
+            [Yy]* ) echo "Teminating the process ...";
+                    ## SIGINT when backup/restore
+                    map=`cat ./mapping`
+                    echo "$map" | while IFS='' read -r line || [[ -n "$line" ]]; do
+                        var2=`echo "$line" | sed -E 's/(.*):(.*):(.*)/\2/'`
+                        echo "Removing file ... $var2"
+                        rm $var2
+                    done
+                    retv=`echo $?`
+                    rm -rf ./mapping 
+                    exit 1;;
+            [Nn]* ) echo break;;
+            * ) echo "Please answer (y/n)";;
+        esac
+    done
+}
+
+function restore_container_status()
+{
+    container="$1"
+    echo "STATUS: $state"
+    if [[ -z "$state" ]]; then
+        echo "state is empty"
+        echo "Default to DOWN STATE"
+    else
+        if [[ $state -eq "0" ]]; then
+            echo "$container should return to DOWN STATE"
+            ret=`docker stop $container`
+            echo "$ret"
+        elif [[ $state -eq "1" ]]; then
+            echo "$container should return to UP STATE"
+            ret=`docker start $container`
+            echo "$ret"
+        else
+            echo "Oooooops~~"
+        fi
+    fi
+}
+
+function set_container_status()
+{
+    container="$1"
+    stat=`docker ps -a --filter name=$1 --format {{.Status}}`
+    if [[ "$stat" =~ "Up" ]]; then
+        state=1
+    elif [[ "$stat" =~ "Exited" ]]; then
+        state=0
+    else
+        echo "Oops~"
+    fi
+}
+
+trap 'stop' SIGINT
+
 mode=$1
 package=$2
 
 case $1 in
-    backup)
-        directory="./backup_tmp"
-        if [ ! -d "$directory" ]; then
-            echo "Generating tempory directory ..."
-            mkdir "$directory"
-        else
-            echo "Cleaning tempory directory ..."
-            rm -rf "$directory"/*
-        fi
-
-        file="./$directory/mapping"
+    backup )
+        file="./mapping"
         if [ -f "$file" ]; then
             echo "Removing the mapping file ..."
             rm "$file"
@@ -22,36 +72,48 @@ case $1 in
 
         i=1
         while [ -n "$3" ]; do
-            # container="$3"
+            set_container_status "$3"
             var=`docker inspect --format='{{range .Mounts}} {{.Source}} {{.Destination}} ,{{end}}' "$3"`
             echo $var | while IFS=" " read -d ',' volume mounted; do
+                echo "Writing log into mapping file ..."
                 echo "$3":"$3"_${i}.tbz:${mounted} >> $file
-                tar -jc -f ./"$directory"/"$3"_${i}.tbz -C ${volume}/../ `basename ${volume}`
+                echo "PACKING: Stopping container -- $3 ..."
+                docker stop $3
+                echo "Packing container ... $3 ... $volume ..."
+                tar -jc -f ./"$3"_${i}.tbz -C ${volume}/../ `basename ${volume}`
                 ((i++))
             done
+            restore_container_status "$3"
             shift 1
         done
 
-        tar -jcv -f "$package" "$directory"
-        rm -rf "$directory"
-    ;;
-    restore)
-        directory="./backup_tmp"
-        if [ -d "$directory" ]; then
-            echo "Cleaning tempory directory ..."
-            rm -rf "$directory"/*
-        #else
-            #echo "Generating tempory directory ..."
-            #mkdir "$directory"
-        fi
+        map=`cat ./mapping`
+        str=""
+        while IFS='' read -r line || [[ -n "$line" ]]; do
+            var2=`echo "$line" | sed -E 's/(.*):(.*):(.*)/\2/'`
+            str="${var2} ${str}"
+        done <<<"$(echo "$map")"
+        echo "MERGING ..."
+        tar -jcv -f "$package" -C ./ $str
 
-		if [ -e "$package" ]; then
+        # Clean
+        while IFS='' read -r line || [[ -n "$line" ]]; do
+            var2=`echo "$line" | sed -E 's/(.*):(.*):(.*)/\2/'`
+            echo "Removing file ... $var2"
+            rm $var2
+        done <<<"$(echo "$map")"
+        echo "Removing file ... mapping"
+        rm mapping
+    ;;
+
+    restore )
+        if [ -e "$package" ]; then
             tar -jxv -f "$package"
         else
             echo "No such file: $package"
         fi
 
-        map=`cat ./backup_tmp/mapping`
+        map=`cat ./mapping`
         echo "$map" | while IFS='' read -r line || [[ -n "$line" ]]; do
             var1=`echo "$line" | sed -E 's/(.*):(.*):(.*)/\1/'`
             var2=`echo "$line" | sed -E 's/(.*):(.*):(.*)/\2/'`
@@ -68,9 +130,8 @@ case $1 in
                         echo "MOUNT: Mounting the volume $var2 to container $var1, $var3"
                         rm -rf `basename ${volume}`
                         echo "MOUNT: Replacing $dictionary/$var2 to ${volume}"
-                        tar -jx -f ./"$directory"/"$var2" -C ${volume}/../ 
+                        tar -jx -f ./"$var2" -C ${volume}/../ 
                         echo "MOUNT: Starting the container $var1"
-                        # docker start $var1
                         res="$res $var1"
                     fi
                 done
@@ -81,7 +142,14 @@ case $1 in
                 echo $retv
             fi
         done
-        rm -rf "$directory"
     ;;
+
+    --h | -help | help )
+        echo "Usage: "
+        echo "    ./backup.sh backup <target_packed_name> <container_name>"
+        echo "    ./backup.sh restore <package_name>"
+    ;;
+
+    * ) break ;;
 esac
 exit 0
